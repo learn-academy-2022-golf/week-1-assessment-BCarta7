1758b474ce625f90235d3772e22381e19698371d
BCarta7 <cannolidude@msn.com>
2022-10-30 12:43:49 -0700

    pushing branch for pull request

--- coding-concepts.js
+++ coding-concepts.js
@@ -15,40 +15,40 @@ const colors = ["tangerine", "magenta", "lilac", "daffodil"]
 // --------------------1) What will this log?
 
 const cohort = "LEARN 2022"
-// console.log(cohort.length)
+console.log(cohort.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: 10
+// b) Verify and explain: .length built-in method will return number of values in the string, including the space.
 
 // --------------------2) What will this log?
 
 const greeting = "Hello World!"
-// console.log(greeting[4])
+console.log(greeting[4])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: o
+// b) Verify and explain: the 4th index value in this string is o, thanks to zero indexing causing it to "count up" from the first balue being 0
 
 // --------------------3) What will this log?
 
 const languages = ["JavaScript", "Ruby", "Python", "C++"]
 const index = 1
-// console.log(languages[index])
+console.log(languages[index])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: "Ruby"
+// b) Verify and explain: Each string is a single value in this array. With the index value of 1 being used, and due to zero indexing, value 1 in this array is the second value, Ruby. I don't know why I thought the quotes would return as well.
 
 // --------------------4) What will this log?
 
 const weekendDays = ["saturday", "sunday"]
-// console.log(weekendDays.toUpperCase())
+console.log(weekendDays.toUpperCase())
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: 2
+// b) Verify and explain: I DID notice .toUpperCase would have issues since the array is two values, not a string itself. This causes an error. I was not sure and guessed it might return an index count, like the earlier .length usage.
 
 // --------------------5) What will this log?
 
 const dataTypes = ["number", "string", "Boolean", "undefined"]
-// console.log(typeof dataTypes.length)
+console.log(typeof dataTypes.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: number
+// b) Verify and explain: typeof will return the data type of the array's values. but using .length in this instance will turn dataTypes into a number, the count of dataTypes values [4]. Thus typeof will return "number"
